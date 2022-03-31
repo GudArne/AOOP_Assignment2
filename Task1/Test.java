e0488f684f7336f5174e12a0cd248f8d3038944c
Mattias <mattias9922@hotmail.com>
2022-03-31 20:23:14 +0200

    Create Test.java

---
+++ Task1/Test.java
@@ -0,0 +1,13 @@
+package Task1;
+
+public class Test{
+
+    int test;
+    Test(int test){
+        this.test = test;
+    }
+    public static void main(String[] args) {
+        Test test = new Test(1);
+    }
+
+}
