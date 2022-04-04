7deb5956d05ef5e95e4d313f61e4c756f07fa1ea
Mattias <mattias9922@hotmail.com>
2022-04-04 21:16:15 +0200

    Added GridBagLayout

--- Task9/Presenter.java
+++ Task9/Presenter.java
@@ -1,18 +1,24 @@
 package Task9;
 import java.awt.*;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 public abstract class Presenter {    
     public void showText(String text) {
+        PresenterWindow.getTextArea().append(text);
 
     }
     public abstract void eastButtonPressed();
     public abstract void westButtonPressed();
 
 
-    public Component createCenterComponent() {
-        return null;
-    // return a Canvas instance
-    // that can display images
+    // Return a JLabel with the image in the given filename
+    public Component createCenterComponent(String file) {
+        ImageIcon icon = new ImageIcon("Task9/Images/shrek.png");
+        JLabel imageLabel = new JLabel(icon);
+        imageLabel.setPreferredSize(new Dimension(550, 400));
+        return imageLabel;
     }
 
     
--- Task9/PresenterWindow.java
+++ Task9/PresenterWindow.java
@@ -2,38 +2,69 @@ package Task9;
 import javax.swing.*;
 import java.awt.*;
 
-public class PresenterWindow extends JFrame {
+public class PresenterWindow extends Presenter {
 
-	private JTextArea jta;
+	private static JTextArea jta;
     private JFrame frame;
     private final Dimension dimension = new Dimension(550, 400);
 
 
 	public PresenterWindow(){
         frame = new JFrame();
-        JPanel panel = new JPanel();
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
         panel.setSize(600, 600);
 
 		jta = new JTextArea(10, 10);
 		jta.append("This is some example text for this presentation.\n");
 
-        ImageIcon icon = new ImageIcon("Task9/shrek.png");
-        JLabel imageLabel = new JLabel(icon);
-        imageLabel.setPreferredSize(dimension);
-        panel.add(imageLabel, BorderLayout.SOUTH);
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 3;
+        c.gridheight = 1;
+        c.fill = GridBagConstraints.BOTH;
+        panel.add(createCenterComponent("Task9/Images/shrek.png"), c);
 
-        frame.add(new JButton("Left"), BorderLayout.SOUTH);
-        frame.add(new JButton("Right"), BorderLayout.SOUTH);
+        c.gridx = 0;
+        c.gridy = 1;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        c.fill = GridBagConstraints.VERTICAL;
+        panel.add(new JButton("Left"), c);
 
-        panel.add(jta, BorderLayout.NORTH);
+        c.gridx = 1;
+        c.gridy = 1;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        panel.add(new JButton("Right"), c);
+
+        c.gridx = 2;
+        c.gridy = 1;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        c.fill = GridBagConstraints.BOTH;
+        panel.add(jta,c);
+
         frame.setSize(600,600);
         frame.add(panel);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
-	public JTextArea getTextArea() {
+	public static JTextArea getTextArea() {
 		return jta;
 	}
+
+    @Override
+    public void eastButtonPressed() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void westButtonPressed() {
+        // TODO Auto-generated method stub
+        
+    }
 
 }
