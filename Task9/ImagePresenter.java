8c98d57796290d565b47963991fa0448f4e3aa72
Mattias <mattias9922@hotmail.com>
2022-04-04 15:08:13 +0200

    Added Task9 files

---
+++ Task9/ImagePresenter.java
@@ -0,0 +1,17 @@
+package Task9;
+
+public abstract class ImagePresenter{
+    public void showImage(String filename){
+        // TODO: implement this method
+
+    }
+    public void showText(String text){
+        // TODO: implement this method
+
+    }
+    public abstract void northButtonPressed();
+    public abstract void eastButtonPressed();
+    public abstract void southButtonPressed();
+    public abstract void westButtonPressed();
+    
+}
--- Task9/Presenter.java
+++ Task9/Presenter.java
@@ -1,5 +1,21 @@
 package Task9;
+import java.awt.*;
 
-public class Presenter {
+public abstract class Presenter {    
+    public void showText(String text) {
+
+    }
+    public abstract void northButtonPressed();
+    public abstract void eastButtonPressed();
+    public abstract void southButtonPressed();
+    public abstract void westButtonPressed();
+
+
+    public Component createCenterComponent() {
+        return null;
+    // return a Canvas instance
+    // that can display images
+    }
+
     
 }
